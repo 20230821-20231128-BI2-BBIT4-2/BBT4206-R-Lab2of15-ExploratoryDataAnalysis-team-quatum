bc37f42bb1ef4497f7f358a4261f78f760afeb83
earlopala <earljac8@gmail.com>
2023-09-07 12:15:38 +0300

    updated Lab2-ExploratoryDataAnalysis.R

--- Lab2-ExploratoryDataAnalysis.R
+++ Lab2-ExploratoryDataAnalysis.R
@@ -24,7 +24,7 @@
 
 # Further reading:
 #   Summary: https://rstudio.github.io/renv/
-#   More detailed article: https://rstudio.github.io/renv/articles/renv.html
+#   More detailed article: https://rstudio.github.io/renv/articles/renv.html 
 
 # Install renv:
 if (!is.element("renv", installed.packages()[, 1])) {
